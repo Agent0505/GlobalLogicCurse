919007e4054aef3d677b3fba016ad7f1dcb6c4c1
Agent0505 <40176253+Agent0505@users.noreply.github.com>
2020-04-13 09:08:09 +0300

    Simple wave generator
    Fixed freq, amp, discretization, phase.

---
+++ Headers.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "Headers.h"
+int main(int argc, char *args[])
+{
+
+	const float pi = M_PI;
+	int A = 10;
+	int freq = 44000;
+	int time = 2;
+
+	ofstream out;
+	out.open("../CSV/sin.csv");
+	if(out.is_open())
+	{
+		for(double i=0; i < 2 * pi * time; i+=pi/(freq*8))
+		{
+			out << sin(i*freq)*A <<","<< endl;
+		}
+	}
+	out.close();
+	out.open("../CSV/triangle.csv");
+	if(out.is_open())
+	{
+		for(double i=0; i < 2*pi * time; i+=pi/180)
+		{
+			out << A*asin(sin(i*freq))/1.5 <<","<< endl;
+		}
+	}
+	out.close();
+	cout << "End of program" << endl;
+	return 0;
+}
